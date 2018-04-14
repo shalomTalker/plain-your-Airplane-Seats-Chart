0a0c0397d66fa1917b4091f85c334b223c3db898
shalom <shalom604@gmail.com>
2018-04-14 10:43:54 +0300

    added validation

--- main.js
+++ main.js
@@ -4,112 +4,143 @@
  {
 	e.preventDefault();
 
-
 	if ($('.grid-panel')) 
 	{
         $('.grid-panel').remove();
     }
 
-	
-
-	var numColumns = Number($('.columns').val());
-	var numRows = Number($('.rows').val());
-
- 	var numPanels = num2Array(numRows * numColumns);
-
-	var arrColumns = num2Array(numColumns);
-	var arrRows = num2Array(numRows);
-
-		var width = numRows * 80;
-		var height = numColumns * 80;
-
-	var gridPanel = $('<div>').addClass("grid-panel").width(width).height(height);
-	for (var i = 0; i < numPanels.length; i++) 
+	if ($('span')) 
 	{
-		var panel = $('<div>', 
-		{
-			text: i + 1 ,
-			class: "panel", 
-			click: function (e)
+        $('span').remove();
+    }
+
+    if ($('.btns')) 
+	{
+        $('.btns').remove();
+        $('bold').remove();
+    }
+	
+	if ($('input').val() == '') 
+	{
+	    var alert = $('<span>', 
+	    {
+	    	text: "**Input can not be left blank",
+			css: 
 			{
-				e.preventDefault();
-				var choseChair = $(event.target);
-				choseChair.toggleClass('red');
+				color: "red",
+				display: "block"
+			}	    	
+	    }).appendTo('form');
+	}
+	else
+	{
+		var numColumns = Number($('.columns').val());
+		var numRows = Number($('.rows').val());
+
+	 	var numPanels = num2Array(numRows * numColumns);
+
+		var arrColumns = num2Array(numColumns);
+		var arrRows = num2Array(numRows);
+
+			var width = numRows * 80;
+			var height = numColumns * 80;
+
+		var gridPanel = $('<div>').addClass("grid-panel").width(width).height(height);
+		for (var i = 0; i < numPanels.length; i++) 
+		{
+			var panel = $('<div>', 
+			{
+				text: i + 1 ,
+				class: "panel", 
+				click: function (e)
+				{
+					e.preventDefault();
+					var choseChair = $(event.target);
+					choseChair.toggleClass('red');
+				},
+			});
+			panel.appendTo(gridPanel);
+			gridPanel.appendTo('.container');
+		}
+
+
+		
+		var btns = $('<div>', 
+		{
+			class: "btns input-group-btn",
+			css: 
+			{
+				width: "100%", 
+				display: "flex", 
+				justifyContent: "center",
+				marginTop: "1rem"
 			},
 		});
-		panel.appendTo(gridPanel);
-		gridPanel.appendTo('.container');
-	}
 
-
-	
-	var btns = $('<div>', 
-	{
-		class: "input-group-btn",
-		css: 
+		var priceBtn = $('<button>', 
 		{
-			width: "100%", 
-			display: "flex", 
-			justifyContent: "center",
-			marginTop: "1rem"
-		},
-	});
-
-	var priceBtn = $('<button>', 
-	{
-		class: "price btn btn-default", 
-		text: "Price",
-		css: {marginRight: "1rem"},
-		click: function (e)
-		{
-
-			if ($('bold')) 
+			class: "price btn btn-default", 
+			text: "Price",
+			css: {marginRight: "1rem"},
+			click: function (e)
 			{
-		        $('bold').remove();
-		    }
-			var onlyPrice = 100;
-			var multiPrice = 200;
-			var totalPrice = 0;
 
-			var numChairs = Number($('*.red').length);
-			console.log(jQuery.type(numChairs));
+				if ($('bold')) 
+				{
+			        $('bold').remove();
+			    }
+				var onlyPrice = 100;
+				var multiPrice = 200;
+				var totalPrice = 0;
 
-			if (numChairs < 2) 
-				{
-					totalPrice = onlyPrice * numChairs;
-				} 
-				else if (numChairs > 1) 
-				{
-					totalPrice = multiPrice * numChairs;
-				}
-				console.log(totalPrice);
-			resPrice = $('<bold>', 
-				{
-					text: "Your calculated price for " + numChairs + " people is " + totalPrice + " $ ." ,
-					css: 
+				var numChairs = Number($('*.red').length);
+				console.log(jQuery.type(numChairs));
+
+				if (numChairs < 2) 
 					{
-						fontSize: "20px",
-						marginTop: "2rem"
+						totalPrice = onlyPrice * numChairs;
+					} 
+					else if (numChairs > 1) 
+					{
+						totalPrice = multiPrice * numChairs;
 					}
-				});
-			resPrice.appendTo('.container');
-		},
-	});
-	priceBtn.appendTo(btns);
+					console.log(totalPrice);
+				resPrice = $('<bold>', 
+					{
+						text: "Your calculated price for " + numChairs + " people is " + totalPrice + " $ ." ,
+						css: 
+						{
+							fontSize: "20px",
+							marginTop: "2rem"
+						}
+					});
+				resPrice.appendTo('.container');
+			},
+		});
+		priceBtn.appendTo(btns);
 
-	var resetBtn = $('<button>', 
-	{
-		class: "reset btn btn-default", 
-		text: "Reset",
-		css: {marginLeft: "1rem"},
-		click: function (e)
+		var resetBtn = $('<button>', 
 		{
-			$('.red').removeClass('red');
-			$('bold').empty();
-		},
-	});
-	resetBtn.appendTo(btns);
-	btns.appendTo('.container');
+			class: "reset btn btn-default", 
+			text: "Reset",
+			css: {marginLeft: "1rem"},
+			click: function (e)
+			{
+				$('.red').removeClass('red');
+				$('bold').empty();
+			},
+		});
+		resetBtn.appendTo(btns);
+
+		if ($('.btns'))
+			{
+		        $('.reset').remove();
+		        $('.price').remove();
+		    }
+
+		btns.appendTo('.container');
+		$('input').val("");
+	}
 });
 function num2Array(num) 
 {
